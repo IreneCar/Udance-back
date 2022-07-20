de2f0c0f1158bc5553d6eb43126f1330379075d6
RubenGH88 <104738166+RubenGH88@users.noreply.github.com>
2022-07-20 11:48:49 +0200

    user model done
    user model done,and prove console erased

--- models/User.model.js
+++ models/User.model.js
@@ -5,6 +5,12 @@ const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
+  image:{String},
+  desctription:{String},
+  danceStyles:[String],
+  receivedLessons:[{ type: Schema.Types.ObjectId, ref: "Lesson" }],
+  givedLessons:[{ type: Schema.Types.ObjectId, ref: "Lesson" }]
+
 });
 
 module.exports = model("User", userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
-  console.log("hola")
   res.json("All good in here");
 });
 
