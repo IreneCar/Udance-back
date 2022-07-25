f024b678bbed7860da0f1068225d6fa9ffb82125
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-25 15:21:34 +0200

    Update user model

--- models/User.model.js
+++ models/User.model.js
@@ -5,12 +5,11 @@ const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
-  image:{String},
-  desctription:{String},
-  danceStyles:[String],
-  receivedLessons:[{ type: Schema.Types.ObjectId, ref: "Lesson" }],
-  givedLessons:[{ type: Schema.Types.ObjectId, ref: "Lesson" }]
-
+  image: { String },
+  description: { String },
+  danceStyles: [String],
+  receivedLessons: [{ type: Schema.Types.ObjectId, ref: "Lesson" }],
+  givedLessons: [{ type: Schema.Types.ObjectId, ref: "Lesson" }],
 });
 
 module.exports = model("User", userSchema);
