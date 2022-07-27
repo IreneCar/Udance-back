57674a225cd743c4c959ecc07fbe7fdaf80c31a4
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-27 17:20:30 +0200

    Update project.routes.js

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -5,28 +5,24 @@ const Lesson = require("../models/Lesson.model");
 const User = require("../models/User.model");
 
 router.get("/lessons", (req, res, next) => {
-console.log("me piden lessons")
-    Lesson.find()
-      .then((allLessons) => res.json(allLessons))
-      .catch((err) => res.json(err));
-  });
+  console.log("me piden lessons");
+  Lesson.find()
+    .then((allLessons) => res.json(allLessons))
+    .catch((err) => res.json(err));
+});
 
+router.get("/lessons/:lessonId", (req, res, next) => {
+  const { lessonId } = req.params;
 
-  router.get("/lessons/:lessonId", (req, res, next) => {
-    const { lessonId } = req.params;
-  
-    if (!mongoose.Types.ObjectId.isValid(lessonId)) {
-      res.status(400).json({ message: "Specified id is not valid" });
-      return;
-    }
-  
-    Lesson.findById(lessonId)
-  
-      .then((lesson) => res.status(200).json(lesson))
-      .catch((error) => res.json(error));
-  });
+  if (!mongoose.Types.ObjectId.isValid(lessonId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
 
+  Lesson.findById(lessonId)
 
-
+    .then((lesson) => res.status(200).json(lesson))
+    .catch((error) => res.json(error));
+});
 
 module.exports = router;
